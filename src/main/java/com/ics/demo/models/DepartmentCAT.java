095657c3e72df15055c9d54dccaf46ff7f56eb0c
Githinji Wanjohi <gyth.wanjohi@gmail.com>
2019-09-25 17:18:43 +0300

    Update with CAT 1 code

--- src/main/java/com/ics/demo/models/DepartmentCAT.java
+++ src/main/java/com/ics/demo/models/DepartmentCAT.java
@@ -12,6 +12,21 @@ public class DepartmentCAT {
         this.companyId = companyId;
     }
 
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
 
     public Long getId() {
         return id;
